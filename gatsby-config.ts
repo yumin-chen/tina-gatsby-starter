4ddab5871f46b2c64b48324910095e1d35047ec2
Jack Pettit [SSW] <57518417+JackDevAU@users.noreply.github.com>
2024-12-12 08:06:50 +1000

    Serve gatsby (#5)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,26 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    // Uncomment and add your site data here
+    // siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [
-    'gatsby-plugin-postcss',
-  ],
+  plugins: ["gatsby-plugin-postcss"],
   headers: [
     {
-      source: '*',
+      source: "*",
       headers: [
         {
-          key: 'Content-Security-Policy',
-          value: 'frame-ancestors *;',
+          key: "Content-Security-Policy",
+          value: "frame-ancestors *;",
         },
       ],
     },
   ],
-}
+};
 
-export default config
+export default config;
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { client } from "../../tina/__generated__/client";
 import { useTina, tinaField } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
-import { navigate } from "gatsby";
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 
 const PostTemplate = ({ serverData }: any) => {
@@ -14,14 +14,12 @@ const PostTemplate = ({ serverData }: any) => {
 
   return (
     <Layout>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-        className="flex items-center bg-gray-800 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-gray-700 transition duration-200 ease-in-out"
+      <Link
+        to="/"
+        className="items-center bg-gray-800 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-gray-700 transition duration-200 ease-in-out"
       >
         🏚️ Go back home
-      </button>
+      </Link>
       <h1 className="text-4xl font-bold my-4">Post page</h1>
       <h1
         className="text-3xl font-semibold my-8 text-orange-400"
